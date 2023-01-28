f11648e98814ab0c087652d5857a0dc37bdad633
Dayaldonadkar <Dayalpython@gmail.com>
2023-01-28 23:32:27 +0530

    first commit

--- src/Components/Data.js
+++ src/Components/Data.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Link, NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 import Show from "./Show";
 
 const Data = () => {
